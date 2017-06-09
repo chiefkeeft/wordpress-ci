e9ada03046bcfb95a9d3a2165408a5c8b2545ffd
chiefkeeft <chiefkeeft@gmail.com>
2017-06-10 02:05:57 +0600

    Create hello.php

--- wp-content/plugins/hello.php
+++ wp-content/plugins/hello.php
@@ -16,6 +16,7 @@ function hello_dolly_get_lyric() {
 	/** These are the lyrics to Hello Dolly */
 	$lyrics = "Hello, Dolly
 Well, hello, Dolly
+111111111111111
 It's so nice to have you back where you belong
 You're lookin' swell, Dolly
 I can tell, Dolly
