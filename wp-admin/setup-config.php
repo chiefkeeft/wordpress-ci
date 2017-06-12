3c3fc0441ea9bd5e28cf5b5c3af95883b48fed89
Zhan <chiefkeeft@gmail.com>
2017-06-12 15:45:24 +0600

    1111

--- wp-admin/setup-config.php
+++ wp-admin/setup-config.php
@@ -144,7 +144,7 @@ switch($step) {
 <h1 class="screen-reader-text"><?php _e( 'Before getting started' ) ?></h1>
 <p><?php _e( 'Welcome to WordPress. Before getting started, we need some information on the database. You will need to know the following items before proceeding.' ) ?></p>
 <ol>
-	<li><?php _e( 'Database name 9999999999999999999999' ); ?></li>
+	<li><?php _e( 'Database name 000000000000000000000' ); ?></li>
 	<li><?php _e( 'Database username' ); ?></li>
 	<li><?php _e( 'Database password' ); ?></li>
 	<li><?php _e( 'Database host' ); ?></li>
